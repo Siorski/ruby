03effcf382c8c0e5a418ace29c7cefc3b2dcba3c
Siorski <kacpersiora@wp.pl>
2012-12-17 15:09:02 +0100

    Walidacja

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,9 +1,12 @@
 class Movie < ActiveRecord::Base
   attr_accessible :dlugosc, :gatunek, :moja_ocena, :ocena_filmweb, :ocena_imdb, :rezyser, :tytul
   
-  
   validates :tytul, presence: true
+<<<<<<< HEAD
   validates :dlugosc, :numericality => { :greater_than_or_equal_to => 0 }  
+=======
+  validates :dlugosc, :numericality => { :greater_than_or_equal_to => 0 }	
+>>>>>>> Walidacja
   validates :ocena_filmweb, :numericality => { only_float: true, :greater_than_or_equal_to => 0  }
   validates :ocena_imdb, :numericality => { only_float: true, :greater_than_or_equal_to => 0  }
 
